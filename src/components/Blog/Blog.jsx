21f054db3b15448c5f81ec9b9a10765effd54b19
St3kl <makalstain@gmail.com>
2026-04-20 10:55:14 +0000

    blog_page adding dinamic fillup

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -5,22 +5,58 @@ import "./Blog.css";
 
 
 const Blog = () => {
+    const blogTopics =[{
+        topicCss: "CSS",
+        topicGrid: "Grid",
+        topicLayout: "Layout",
+        topicFrontend: "Frontend",
+        topicWebDesign: "Web design",
+    }];
+    const postAuthor = [
+        {
+            author: "LH",
+            name: "Luis Huesca",
+            bio: "Front-end developer & UI enthusiast. Writing about CSS, JS, and building better web experiences."
+        }
+    ];
+    const relatedArticles = [
+        {
+            artTitle: "//css",
+            artInfo: "Flexbox vs Grid: when to use which",
+            artDate: "Mar 28, 2025"
+        },
+        {
+            artTitle: "//JavaScript",
+            artInfo: "Async/await patterns you should know",
+            artDate: "Mar 14, 2025"
+        },
+        {
+            artTitle: "//React",
+            artInfo: "Component design for scalable UIs",
+            artDate: "Feb 20, 2025"
+        },
+        {
+            artTitle: "//Node.js",
+            artInfo: "Backend best practices for modern web apps",
+            artDate: "May 20, 2025"
+        },
+    ];
  return (
     <div className="blog_page">
         <div className="blog_topics">
-            <div className="blog_topic">CSS</div>
-            <div className="blog_topic">Grid</div>
-            <div className="blog_topic">layout</div>
-            <div className="blog_topic">frontend</div>
-            <div className="blog_topic">web design</div>
+            <div className="blog_topic">{blogTopics[0].topicCss}</div>
+            <div className="blog_topic">{blogTopics[0].topicGrid}</div>
+            <div className="blog_topic">{blogTopics[0].topicLayout}</div>
+            <div className="blog_topic">{blogTopics[0].topicFrontend}</div>
+            <div className="blog_topic">{blogTopics[0].topicWebDesign}</div>
         </div>
         <div className="blog_newest">
             <div className="newest">
-                <div className="postAuth">LH</div>
+                <div className="postAuth">{postAuthor[0].author}</div>
                 <div className="post_info">
-                    <div className="authName">Luis Huesca</div>
+                    <div className="authName">{postAuthor[0].name}</div>
                     <div className="post_resume">
-                        Front-end developer & UI enthusiast. Writing about CSS, JS, and building better web experiences.
+                        {postAuthor[0].bio}
                     </div>
                 </div>
             </div>
@@ -29,25 +65,32 @@ const Blog = () => {
             <div className="relate_title">Related articles</div>
             <div className="related_articles">
                 {/* ========= */}
-                <div className="relate_article">
-                    <div className="art_titl">//css</div>
-                    <div className="art_info">Flexbox vs Grid: when to use which</div>
-                    <div className="art_date">Mar 28, 2025</div>
-                </div>
+                {relatedArticles.map((article, index) => (
+                    <div className="relate_article" key={index}>
+                        <div className="art_titl">{article.artTitle}</div>
+                        <div className="art_info">{article.artInfo}</div>
+                        <div className="art_date">{article.artDate}</div>
+                    </div>
+                ))};
+                {/* <div className="relate_article">
+                    <div className="art_titl">{relatedArticles[0].artTitle}</div>
+                    <div className="art_info">{relatedArticles[0].artInfo}</div>
+                    <div className="art_date">{relatedArticles[0].artDate}</div>
+                </div> */}
                 {/* ========= */}
                 {/* ========= */}
-                <div className="relate_article">
-                    <div className="art_titl">//JavaScript</div>
-                    <div className="art_info">Async/await patterns you should know</div>
-                    <div className="art_date">Mar 14, 2025</div>
-                </div>
+                {/* <div className="relate_article">
+                    <div className="art_titl">{relatedArticles[1].artTitle}</div>
+                    <div className="art_info">{relatedArticles[1].artInfo}</div>
+                    <div className="art_date">{relatedArticles[1].artDate}</div>
+                </div> */}
                 {/* ========= */}
                 {/* ========= */}
-                <div className="relate_article">
-                    <div className="art_titl">//React</div>
-                    <div className="art_info">Component design for scalable UIs</div>
-                    <div className="art_date">Feb 20, 2025</div>
-                </div>
+                {/* <div className="relate_article">
+                    <div className="art_titl">{relatedArticles[2].artTitle}</div>
+                    <div className="art_info">{relatedArticles[2].artInfo}</div>
+                    <div className="art_date">{relatedArticles[2].artDate}</div>
+                </div> */}
                 {/* ========= */}
             </div>
         </div>
